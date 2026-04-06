761bdf4762c53ec766a2a1796670beb4e134a500
mayageva2 <mayageva7@gmail.com>
2026-04-06 11:41:19 +0300

    creating BinaryNumber class

---
+++ Ex01 Maya 322854415 Ben 323918771/Ex01_01/BinaryNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_01
+{
+    internal class BinaryNumber
+    {
+        private string m_BinaryNumberString;
+        private int m_DecimalValueOfBinaryNumber;
+
+        public BinaryNumber(string BinaryNumberString)
+        {
+            m_BinaryNumberString = BinaryNumberString;
+        }
+
+        public static bool isValid(string binaryNumberString, int LengthOfBinaryNumber)
+        {
+            if (binaryNumberString.Length < LengthOfBinaryNumber)
+            {
+                System.Console.Error.WriteLine("Invalid input size. Please enter a 7 digit binary number");
+                return false;
+            }
+
+            for (int i = 0; i < LengthOfBinaryNumber; ++i)
+            {
+                if (binaryNumberString[i] != '0' && binaryNumberString[i] != '1')
+                {
+                    System.Console.Error.WriteLine("Invalid input character. Please enter only 0 or 1");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- Ex01 Maya 322854415 Ben 323918771/Ex01_01/Program.cs
+++ Ex01 Maya 322854415 Ben 323918771/Ex01_01/Program.cs
@@ -8,5 +8,31 @@ namespace Ex01_01
 {
     internal class Program
     {
+        protected const int k_AmountOfBinaryNumbers = 4;
+        protected const int k_LengthOfBinaryNumbers = 7;
+
+        public static void Main()
+        {
+            BinaryNumber[] arrayOfBinaryNumbersFromUser = new BinaryNumber[k_AmountOfBinaryNumbers];
+
+            for (int i = 0; i < k_AmountOfBinaryNumbers; ++i)
+            {
+                string input = System.Console.ReadLine();
+                if (BinaryNumber.isValid(input, k_LengthOfBinaryNumbers))
+                {
+                    arrayOfBinaryNumbersFromUser[i] = new BinaryNumber(input);
+                }
+                else
+                {
+                    --i;
+                }
+
+
+
+
+
+
+            }
+        }
     }
 }
