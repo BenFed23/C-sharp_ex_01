c5d0a19e970a49acc55e21968eeaac045c1939e8
Ben Fediuck <benfe@mta.ac.il>
2026-04-26 00:04:52 +0300

    I removed all the classes exept Program and put all the methods into Program.

--- Ex01 Maya 322854415 Ben 323918771/Ex01_04/Program.cs
+++ Ex01 Maya 322854415 Ben 323918771/Ex01_04/Program.cs
@@ -4,37 +4,37 @@
 
 namespace Ex01_04
 {
-    internal class Program
+    public class Program
     {
         public static void Main()
         {
             Console.WriteLine("Enter a 8 cherecters string ");
-            string i_Sentence = Console.ReadLine();
-            if (i_Sentence.Length != 8)
+            string sentence = Console.ReadLine();
+            if (sentence.Length != 8)
             {
                 Console.WriteLine("The string needs to be 8 characters long, try again!");
                 return;
             }
-            bool strIsPolindrom = StringChecker.IsPolindrom(i_Sentence);
+            bool strIsPalindrome = IsPalindrome(sentence);
             string analysisResult = "";
-            if (StringChecker.IsAllNumbers(i_Sentence))
+            if (IsAllNumbers(sentence))
             {
-                AllNumbers number = new AllNumbers(i_Sentence);
-                bool isDivisibleBy4 = number.IsDivideBy(4);
+                int.TryParse(sentence, out int RepresentativeNum);
+                bool isDivisibleBy4 = IsDivideBy(4, RepresentativeNum);
                 analysisResult = string.Format("The number is divisible by 4: {0}", isDivisibleBy4);
             }
-            else if (StringChecker.IsAllLetters(i_Sentence))
+            else if (IsAllLetters(sentence))
             {
-                AllLetters letters = new AllLetters(i_Sentence);
-                int uppersCount = letters.uppercaseCount();
-                bool isDescending = letters.Isdescendingorder();
+               
+                int uppersCount = UppercaseCount(sentence);
+                bool isDescending = Isdescendingorder(sentence);
                 analysisResult = string.Format("Uppercase count: {0}\nIs descending series: {1}", uppersCount, isDescending);
             }
             string finalOutput = string.Format(
-            
+
             "Is Palindrome: {0}{2}" +
            "{1}",
-            strIsPolindrom,
+            strIsPalindrome,
             analysisResult,
             Environment.NewLine);
             Console.WriteLine(finalOutput);
@@ -43,6 +43,98 @@ namespace Ex01_04
 
 
         }
+        public static bool IsAllNumbers(string i_InputStr)
+        {
+            bool isAllNunbers = true; ;
+            for (int i = 0; i < i_InputStr.Length; i++)
+            {
+                if (i == 0 && i_InputStr[i] == '-' && i_InputStr.Length > 1)
+                    continue;
+
+                if (!char.IsDigit(i_InputStr[i]))
+                {
+                    isAllNunbers = false;
+                    return isAllNunbers;
+                }
+            }
+            return isAllNunbers;
+        }
+        public static bool IsAllLetters(string i_InputStr)
+        {
+            bool isAllLetters = true;
+            for (int i = 0; i < i_InputStr.Length; i++)
+            {
+                if (!char.IsLetter(i_InputStr[i]))
+                {
+                    isAllLetters = false;
+                    return isAllLetters;
+                }
+            }
+            return isAllLetters;
+        }
+        public static bool IsPalindrome(string i_InputStr)
+        {
+            bool isPalindrome = true;
+            if (i_InputStr.Length == 0)
+            {
+                return isPalindrome;
+            }
+            else if (i_InputStr[0] != i_InputStr[i_InputStr.Length - 1])
+            {
+                isPalindrome = false;
+                return isPalindrome;
+            }
+            else
+            {
+                string smallerStr = i_InputStr.Substring(1, i_InputStr.Length - 2);
+                return IsPalindrome(smallerStr);
+            }
+
+
+        }
+        public static bool IsDivideBy(int i_dividerNum, int i_RepresentativeNum)
+        {
+            bool isdividedbynum = true;
+            if (i_RepresentativeNum % i_dividerNum != 0)
+            {
+                isdividedbynum=false;
+                return isdividedbynum;
+            }
+            return isdividedbynum;
+        }
+        public static bool Isdescendingorder(string i_RepresentativeStr)
+        {
+            bool isdescendingorder = true;
+            for (int i = 1; i < i_RepresentativeStr.Length; i++)
+            {
+                char firstCharacter = char.ToLower(i_RepresentativeStr[i - 1]);
+                char secondCharacter = char.ToLower(i_RepresentativeStr[i]);
+                if (firstCharacter - secondCharacter <= 0)
+                {
+                    isdescendingorder = false;
+                    return isdescendingorder;
+                }
+            }
+            return isdescendingorder;
+        }
+        public static int UppercaseCount(string i_RepresentativeStr)
+        {
+            int uppercaseCounter = 0;
+            for (int i = 0; i < i_RepresentativeStr.Length; i++)
+            {
+                if (char.IsUpper(i_RepresentativeStr[i]))
+                {
+                    uppercaseCounter++;
+                }
+            }
+            return uppercaseCounter;
+        }
+        
+        
+        
+    
+        
+
     }
     
 }
--- Ex01 Maya 322854415 Ben 323918771/Ex01_05/Program.cs
+++ Ex01 Maya 322854415 Ben 323918771/Ex01_05/Program.cs
@@ -1,6 +1,5 @@
-﻿using Ex01_04;
-using System;
-
+﻿using System;
+using System.Text;
 
 namespace Ex01_05
 {
@@ -12,33 +11,137 @@ namespace Ex01_05
             string userInput = Console.ReadLine();
             if ((userInput == null || (userInput.Length != 9))) 
             {
-                Console.WriteLine("the number is not 9 digits long,try again!");
+                Console.WriteLine("The number is not 9 digits long,Try again!");
                 return;
             }
-            if (!StringChecker.IsAllNumbers(userInput))
+            if (!IsAllNumbers(userInput))
             {
-                Console.WriteLine("enter only numbers!");
+                Console.WriteLine("Enter only numbers!");
                 return;
             }
-            Number number=new Number(userInput);
-            string biggerThanUnitNum = "";
-            int count = number.BiggerThanunitsDigit(out string list);
+           
+            
+            int count = BiggerThanUnitsDigit(out string list,userInput);
             string output = string.Format(
-            "The units digit is {0}.The digit that are greather than it:{2}. all in all: {1}{5}" +
+            "The units digit is {0}.The digit that are greater than it:{2}. toal: {1}{5}" +
             "How many digits are divisible by 4? {3}{5}" +
             "The multiplication of the largest and smallest digit: {4}{5}" +
             "How many unique digits: {6}",
             userInput[userInput.Length - 1],
             count,
             list,
-            number.dividedby4(),                 
-            number.getMultMaxandMinNum(),       
+            DividedBy4(userInput),                 
+            GetMultMaxAndMinNum(userInput),       
             Environment.NewLine,                
-            number.findUnique()                 
+            FindUnique(userInput)                 
             );
             Console.WriteLine(output);
 
 
-        }    
+        }
+        private static int getDigitAt(int i_Index,string i_RepresentativeStr)
+        {
+            return i_RepresentativeStr[i_Index] - '0';
+        }
+        public static int BiggerThanUnitsDigit(out string o_DigitsList,string i_RepresentativeStr)
+        {
+            int.TryParse(i_RepresentativeStr, out int RepresentativeNum);
+            StringBuilder listOfBiggerNums = new StringBuilder();
+            int biggerThanCount = 0;
+            int unitsDigit = RepresentativeNum % 10;
+            for (int i = 0; i < i_RepresentativeStr.Length; i++)
+            {
+                int currentdigit = getDigitAt(i,i_RepresentativeStr);
+                if (currentdigit > unitsDigit)
+                {
+                    if (listOfBiggerNums.Length > 0)
+                    {
+                        listOfBiggerNums.Append(",");
+                    }
+                    listOfBiggerNums.Append(i_RepresentativeStr[i]);
+                    biggerThanCount++;
+                }
+            }
+
+            if (listOfBiggerNums.Length > 0)
+            {
+                o_DigitsList = listOfBiggerNums.ToString();
+            }
+            else
+            {
+                o_DigitsList = "";
+            }
+            return biggerThanCount;
+        }
+        public static int FindUnique(string i_RepresentativeStr)
+        {
+            int numOfUnique = 0;
+            for (int i = 0; i < i_RepresentativeStr.Length; i++)
+            {
+                bool alreadyCounted = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (i_RepresentativeStr[i] == i_RepresentativeStr[j])
+                    {
+                        alreadyCounted = true;
+                        break;
+                    }
+                }
+                if (!alreadyCounted)
+                {
+                    numOfUnique++;
+                }
+
+            }
+            return numOfUnique;
+        }
+        public static int GetMultMaxAndMinNum(string i_RepresentativeStr)
+        {
+            int max = getDigitAt(0,i_RepresentativeStr);
+            int min = getDigitAt(0,i_RepresentativeStr);
+            for (int i = 0; i < i_RepresentativeStr.Length; i++)
+            {
+                int currentDigit = getDigitAt(i,i_RepresentativeStr);
+                if (currentDigit > max)
+                {
+                    max = currentDigit;
+                }
+                if (currentDigit < min)
+                {
+                    min = currentDigit;
+                }
+
+            }
+            return max * min;
+        }
+        public static int DividedBy4(string i_RepresentativeStr)
+        {
+            int dividedby4Count = 0;
+            for (int i = 0; i < i_RepresentativeStr.Length; i++)
+            {
+                int currentDig = getDigitAt(i,i_RepresentativeStr);
+                if (currentDig % 4 == 0)
+                {
+                    dividedby4Count++;
+                }
+            }
+            return dividedby4Count;
+        }
+        public static bool IsAllNumbers(string i_InputStr)
+        {
+            bool isAllNumbers = true; 
+            for (int i = 0; i < i_InputStr.Length; i++)
+            {
+                if (i == 0 && i_InputStr[i] == '-' && i_InputStr.Length > 1)
+                    continue;
+
+                if (!char.IsDigit(i_InputStr[i]))
+                {
+                    isAllNumbers = false;
+                    return isAllNumbers;
+                }
+            }
+            return isAllNumbers;
+        }
     }
 }
